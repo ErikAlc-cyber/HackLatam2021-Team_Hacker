58912fc4fd792f250ed07f5f6ab38780b664bc5d
ErikAlc-cyber <ealcantarac100@alumno.ipn.mx>
2021-02-21 09:09:01 -0600

    Updates to projuect

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="Prueba",  # Nombre
+    version="0.1",  # Versión de desarrollo
+    description="Proyecto de HackLatam",  # Descripción del funcionamiento
+    author="Team_Hack",  # Nombre del autor
+    author_email='hfofficial.me',  # Email del autor
+    license="MIT",  # Licencia: MIT, GPL, GPL 2.0...
+    install_requires=["pygame", "numpy"],
+)
